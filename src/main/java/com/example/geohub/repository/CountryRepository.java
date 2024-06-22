3e3574a57302c41696f14b010cddc0fd7fd364d3
manojtekdevs <manojtek369.gist@gmail.com>
2024-06-22 08:49:23 +0000

    first commit

---
+++ src/main/java/com/example/geohub/repository/CountryRepository.java
@@ -0,0 +1,16 @@
+package com.example.geohub.repository;
+
+import com.example.geohub.model.Country;
+import java.util.*;
+
+public interface CountryRepository {
+    ArrayList<Country> getCountries();
+
+    Country getCountryById(int countryId);
+
+    Country addCountry(Country country);
+
+    Country updateCountry(int countryId, Country country);
+
+    void deleteCountry(int countryId);
+}
